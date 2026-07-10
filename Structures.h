0085ce3520180c8ad493614b33b04003d03c2f24
AnastasiaKamienienko <kib25-a.kamienienko@nubip.edu.ua>
2026-07-10 15:12:08 +0300

    Create Structures.h

---
+++ Structures.h
@@ -0,0 +1,36 @@
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
+
+#include <string>
+#include <vector>
+
+struct Vacancy {
+    int id;
+    std::string title;       
+    std::string company;     
+    std::string description; 
+    double salary;           
+};
+
+struct Application {
+    int vacancyId;
+    std::string candidateName;
+    std::string resumeText; 
+    std::string status;      
+};
+
+
+struct Candidate {
+    std::string fullName;
+    std::string email;
+    std::string skills;      
+};
+
+
+struct Employer {
+    std::string companyName;
+    std::string industry;   
+    std::string contactInfo;
+};
+
+#endif 
